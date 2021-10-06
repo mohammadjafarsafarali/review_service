9fd42acc5a805dcc0081b450a1398c0fb1974d4e
mohammad safarali <mjsafarali@gmail.com>
2021-10-06 22:36:16 +0330

    feat: migrations for options and comments added

---
+++ database/migrations/2021_10_06_184348_create_options_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOptionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('options', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('product_id');
+            $table->index('product_id');
+            $table->boolean('product_visibility');
+            $table->enum('comments_mode', ['public', 'consumer', 'deactive']);
+            $table->enum('vote_mode', ['public', 'consumer', 'deactive']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('options');
+    }
+}
