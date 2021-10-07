803663d586ccb08d91e6b40ca9d0267e26d744bf
mohammad safarali <mjsafarali@gmail.com>
2021-10-07 14:29:34 +0330

    docs: documents fixed in use methods

--- app/Http/Controllers/Api/v1/OptionsController.php
+++ app/Http/Controllers/Api/v1/OptionsController.php
@@ -2,10 +2,9 @@
 
 namespace App\Http\Controllers\Api\v1;
 
-use App\Http\Controllers\Controller;
 use App\Http\Resources\Api\v1\OptionsResource;
+use App\Http\Controllers\Controller;
 use App\Services\OptionsService;
-use Illuminate\Http\Request;
 
 class OptionsController extends Controller
 {
--- app/Http/Resources/Api/v1/OptionsResource.php
+++ app/Http/Resources/Api/v1/OptionsResource.php
@@ -2,9 +2,8 @@
 
 namespace App\Http\Resources\Api\v1;
 
-use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Http\Request;
 
 class OptionsResource extends JsonResource
 {
