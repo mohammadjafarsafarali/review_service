eb5adfcec059131f1f9d309726822f14c0f24b24
mohammad safarali <mjsafarali@gmail.com>
2022-01-30 02:10:40 +0330

    feat: integrate base model with exception handler

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Supports\ApiResponse;
+use App\Supports\ExceptionHandler;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -9,5 +11,15 @@ use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests, ApiResponse;
+
+    /**
+     * @var ExceptionHandler
+     */
+    protected $exceptionHandler;
+
+    public function __construct()
+    {
+        $this->exceptionHandler = new ExceptionHandler();
+    }
 }
